fb5c869aed422d0d1de6c5bec4e0d310b76f7b1f
ogungbile <52929020+ogungbile@users.noreply.github.com>
2022-12-15 23:56:52 +0100

    Update pchar.c

--- pchar.c
+++ pchar.c
@@ -19,11 +19,13 @@ void pchar(stack_t **stack, unsigned int line_cnt)
 		exit(EXIT_FAILURE);
 		return;
 	}
+	/*
 	if (isascii((*stack)->n) == 0)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_cnt);
 		exit(EXIT_FAILURE);
 		return;
 	}
+	*/
 	printf("%c\n", (*stack)->n);
 }
